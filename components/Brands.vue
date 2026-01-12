4fee1b82cebecb0251a8c82cc4dbaa9b4919ad88
umbra <vitali.slepnyov@gmail.com>
2026-01-12 23:53:01 +0300

    feat:  добавить компонент Brands

---
+++ components/Brands.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <section class="w-[320px] h-[400px] bg-[#FFF1E8] border-4 border-black rounded-xl max-[801px]:hidden">
+        <h3 class="border-b-2 border-black py-2 text-lg text-center">Все Бренды</h3>
+        <ul class="mt-2 text-center">
+            <li>Filippo Sorcinelli</li>
+            <li>Etat Libre d'Orange</li>
+            <li>Orto Parisi</li>
+            <li>Остатки во флаконах</li>
+            <li>Услуги / Встречи</li>
+        </ul>
+    </section>
+</template>
