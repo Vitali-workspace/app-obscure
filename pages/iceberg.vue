fdcaa573b8c8b509c8a9dd44593e97c87f0f6ca2
umbra <vitali.slepnyov@gmail.com>
2026-01-22 23:47:59 +0300

    feat:  добавить страницу с айсбергом

--- pages/iceberg.vue
+++ pages/iceberg.vue
@@ -5,6 +5,37 @@
 
 <template>
     <section>
+        <div>
+            <p>Интро айсберга</p>
+        </div>
+        <div>
+            <h2>Уровеннь 1</h2>
+            <p>Описание уровня</p>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
 
+        <div>
+            <h2>Уровеннь 2</h2>
+            <p>Описание уровня</p>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+
+        <div>
+            <h2>Уровеннь 3</h2>
+            <p>Описание уровня</p>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
     </section>
 </template>
