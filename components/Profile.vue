cec676c1aaaeb1a25b643e4367d8d0ab07695ad4
umbra <vitali.slepnyov@gmail.com>
2026-01-26 23:50:03 +0300

    feat:  добавить поля ввода

--- components/Profile.vue
+++ components/Profile.vue
@@ -2,21 +2,44 @@
 
 <template>
     <section>
-        <p>Адресс доставки</p>
+        <h2>Адресс доставки</h2>
         <ul>
-            <li>Имя</li>
-            <li>Фамилия</li>
-            <li>Город доставки
-                <input type="text">
+            <li>
+                <label for="name">Имя</label>
+                <input type="text" placeholder="Имя" id="name">
             </li>
-            <li>Улица и номер дома</li>
-            <li>Почтовый индекс Белпочты</li>
-            <li>Данные Европочты</li>
-            <li>Номер телефона</li>
-            <li>Тег в телеграм</li>
+            <li>
+                <label for="surname">Фамилия</label>
+                <input type="text" placeholder="Фамилия" id="surname">
+            </li>
+            <li>
+                <label for="delivery-city">Город доставки</label>
+                <input type="text" placeholder="Минск" id="delivery-city">
+            </li>
+            <li>
+                <label for="delivery-address">Адрес и номер дома</label>
+                <input type="text" placeholder="ул Ленина, 44" id="delivery-address">
+            </li>
+            <li>
+                <label for="belposhta-index">Почтовый индекс Белпочты</label>
+                <input type="text" placeholder="220011" id="belposhta-index">
+            </li>
+            <li>
+                <label for="europost-address">Адрес европочты</label>
+                <input type="text" placeholder="220011" id="europost-address">
+            </li>
+            <li>
+                <label for="phone-number">Номер телефона</label>
+                <input type="text" placeholder="+375..." id="phone-number">
+            </li>
+            <li>
+                <label for="telegram-tag">Тег в телеграм</label>
+                <input type="text" placeholder="@name" id="telegram-tag">
+            </li>
         </ul>
-        <button>Сохранить данные</button>
+        <button class="">Сохранить данные</button>
     </section>
+
 
     <section>
         <p>История заказов</p>
