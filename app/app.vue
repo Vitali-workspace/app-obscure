d30bc317e3947d4f3dec5297bd741b270dbcaa9a
umbra <vitali.slepnyov@gmail.com>
2026-02-01 23:56:13 +0300

    refactor:  изменение размеров блока

--- app/app.vue
+++ app/app.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="flex min-[801px]:justify-center h-screen bg-[#FFB904]">
+  <div class="flex min-[801px] justify-center h-screen bg-[#FFB904]">
     <!-- <NuxtRouteAnnouncer />
     <NuxtWelcome /> -->
 
     <!-- ! рамку из мейн убрать -->
     <!-- ! мейн по высоте экрана -->
-    <main class="flex w-[clamp(300px,90vw,1800px)] max-[801px]:flex-col max-[801px]:m-0 m-4">
+    <main class="flex w-[clamp(500px,99vw,1800px)] max-[801px]:flex-col max-[801px]:m-0 m-4 overflow-hidden">
 
       <!-- ! Бренды -->
-      <section class="w-[320px] h-[400px] bg-[#FFF1E8] border-4 border-black rounded-xl max-[801px]:hidden">
+      <!-- <Brands /> -->
+
+      <section class="min-w-[220px] h-[400px] bg-[#FFF1E8] border-4 border-black rounded-xl max-[1001px]:hidden">
         <h3 class="border-b-2 border-black py-2 text-lg text-center">Все Бренды</h3>
         <ul class="mt-2 text-center">
           <li>Filippo Sorcinelli</li>
@@ -16,40 +18,72 @@
           <li>Orto Parisi</li>
           <li>Остатки во флаконах</li>
           <li>Услуги / Встречи</li>
+          <li>Все</li>
         </ul>
       </section>
 
-
       <!-- ! Магазин -->
-
+      <Shop />
 
 
       <!-- ! Корзина -->
-      <section class="w-[320px] h-[400px] bg-[#FFF1E8] border-4 border-black rounded-xl max-[801px]:hidden">
+      <Cart />
 
-        <h3 class="border-b-2 border-black py-2 text-lg text-center">Корзина</h3>
-        <ul class="mt-2 text-center">
-          <li>
-            <div class="flex w-[300px] border-2 border-black rounded-xl mx-2 my-3">
-              <!-- фотка -->
-              <NuxtImg class="rounded-lg aspect-square w-full max-w-[50px]" :src="imageUrl" />
-              <div class="flex flex-col ml-2">
-                <h3>Borntostandout</h3>
-                <h4>Naked Laundry</h4>
+      <!-- <section></section> -->
+
+
+      <!-- <section class="max-[801px]:hidden">
+        <div class="w-[320px] h-[400px] bg-[#FFF1E8] border-4 border-black rounded-xl">
+
+          <h3 class="border-b-2 border-black py-2 text-lg text-center">Корзина</h3>
+          <ul class="mt-2 text-center">
+            <li>
+              <div class="flex w-[300px] border-2 border-black rounded-xl mx-2 my-3">
+                
+                <NuxtImg class="rounded-lg aspect-square w-full max-w-[50px]" :src="imageUrl" />
+                <div class="flex flex-col ml-2">
+                  <h3>Borntostandout</h3>
+                  <h4>Naked Laundry</h4>
+                </div>
+                <p class="ml-2"><span>5 руб</span><sup> 2 мл</sup></p>
+                <button class="border-l-2 border-black ml-auto px-4">x</button>
               </div>
-              <p class="ml-2"><span>5 руб</span><sup> 2 мл</sup></p>
-              <button class="border-l-2 border-black ml-auto px-4">x</button>
-            </div>
-          </li>
-        </ul>
+            </li>
+            <li>
+              <div class="flex w-[300px] border-2 border-black rounded-xl mx-2 my-3">
+                
+                <NuxtImg class="rounded-lg aspect-square w-full max-w-[50px]" :src="imageUrl" />
+                <div class="flex flex-col ml-2">
+                  <h3>Borntostandout</h3>
+                  <h4>Naked Laundry</h4>
+                </div>
+                <p class="ml-2"><span>5 руб</span><sup> 2 мл</sup></p>
+                <button class="border-l-2 border-black ml-auto px-4">x</button>
+              </div>
+            </li>
+          </ul>
+        </div>
 
-        <!-- кнопки для профиля -->
-      </section>
+
+        
+        <div class="flex flex-col mt-8">
+          <button @click="" class="bg-[#FFF1E8] border-4 border-black rounded-xl">
+            Войти / Выйти
+          </button>
+          <button @click="" class="bg-[#FFF1E8] border-4 border-black rounded-xl">
+            Профиль
+          </button>
+          <button @click="" class="bg-[#FFF1E8] border-4 border-black rounded-xl">
+            История покупок
+          </button>
+        </div>
+
+      </section> -->
 
 
     </main>
     <nav
-      class="flex bottom-0 w-screen justify-around fixed text-lg pb-8 pt-4 bg-[#FFB904] min-[801px]:hidden border-t-4 border-black">
+      class="flex bottom-0 w-screen justify-around fixed text-lg pb-8 pt-4 bg-[#FFB904] min-[1001px]:hidden border-t-4 border-black">
       <button class="mx-4">Бренды</button>
       <button class="">Магазин</button>
       <button class="ml-4">Корзина</button>
@@ -61,7 +95,9 @@
 
 
 <script setup lang="ts">
-
+import Shop from '../components/Shop.vue';
+import Brands from '../components/Brands.vue'
+import Cart from '../components/ShoppingCart.vue'
 
 const imageUrl = 'https://lulua.pl/wp-content/uploads/2024/10/perfumeria_lulua_naked_laundry_btso-900x900.jpg';
 </script>
