8b685d4ef4e17d04f8c4322500b9b14566d328e1
umbra <vitali.slepnyov@gmail.com>
2026-02-06 23:52:17 +0300

    refactor:  изменить списки брендов

--- app/app.vue
+++ app/app.vue
@@ -8,19 +8,19 @@
     <main class="flex w-[clamp(500px,99vw,1800px)] max-[801px]:m-0 m-4 overflow-hidden">
 
       <!-- ! Бренды -->
-      <!-- <Brands /> -->
+      <Brands />
 
-      <section class="min-w-[220px] h-[400px] bg-[#FFF1E8] border-4 border-black rounded-xl max-[1001px]:hidden">
+      <!-- <section class="min-w-[220px] h-[400px] bg-[#FFF1E8] border-4 border-black rounded-xl max-[1001px]:hidden">
         <h3 class="border-b-2 border-black py-2 text-lg text-center">Все Бренды</h3>
         <ul class="mt-2 text-center">
           <li>Filippo Sorcinelli</li>
-          <li>Etat Libre d'Orange</li>
-          <li>Orto Parisi</li>
+          <li class="hidden">Etat Libre d'Orange</li>
+          <li>Наборы пробников</li>
           <li>Остатки во флаконах</li>
-          <li>Услуги / Встречи</li>
+          <li class="hidden">Услуги / Встречи</li>
           <li>Все</li>
         </ul>
-      </section>
+      </section> -->
 
       <!-- ! Магазин -->
       <Shop />
@@ -29,65 +29,14 @@
       <!-- ! Корзина -->
       <Cart />
 
-      <!-- <section></section> -->
-
-
-      <!-- <section class="max-[801px]:hidden">
-        <div class="w-[320px] h-[400px] bg-[#FFF1E8] border-4 border-black rounded-xl">
-
-          <h3 class="border-b-2 border-black py-2 text-lg text-center">Корзина</h3>
-          <ul class="mt-2 text-center">
-            <li>
-              <div class="flex w-[300px] border-2 border-black rounded-xl mx-2 my-3">
-                
-                <NuxtImg class="rounded-lg aspect-square w-full max-w-[50px]" :src="imageUrl" />
-                <div class="flex flex-col ml-2">
-                  <h3>Borntostandout</h3>
-                  <h4>Naked Laundry</h4>
-                </div>
-                <p class="ml-2"><span>5 руб</span><sup> 2 мл</sup></p>
-                <button class="border-l-2 border-black ml-auto px-4">x</button>
-              </div>
-            </li>
-            <li>
-              <div class="flex w-[300px] border-2 border-black rounded-xl mx-2 my-3">
-                
-                <NuxtImg class="rounded-lg aspect-square w-full max-w-[50px]" :src="imageUrl" />
-                <div class="flex flex-col ml-2">
-                  <h3>Borntostandout</h3>
-                  <h4>Naked Laundry</h4>
-                </div>
-                <p class="ml-2"><span>5 руб</span><sup> 2 мл</sup></p>
-                <button class="border-l-2 border-black ml-auto px-4">x</button>
-              </div>
-            </li>
-          </ul>
-        </div>
-
-
-        
-        <div class="flex flex-col mt-8">
-          <button @click="" class="bg-[#FFF1E8] border-4 border-black rounded-xl">
-            Войти / Выйти
-          </button>
-          <button @click="" class="bg-[#FFF1E8] border-4 border-black rounded-xl">
-            Профиль
-          </button>
-          <button @click="" class="bg-[#FFF1E8] border-4 border-black rounded-xl">
-            История покупок
-          </button>
-        </div>
-
-      </section> -->
-
 
     </main>
     <nav
       class="flex bottom-0 w-screen justify-around fixed text-lg pb-8 pt-4 bg-[#FFB904] min-[1001px]:hidden border-t-4 border-black">
-      <button class="mx-4">Бренды</button>
-      <button class="">Магазин</button>
-      <button class="ml-4">Корзина</button>
-      <button class="mx-4">Профиль</button>
+      <button @click="" class="mx-4">Бренды</button>
+      <button @click="" class="">Магазин</button>
+      <button @click="" class="ml-4">Корзина</button>
+      <button @click="" class="mx-4">Профиль</button>
     </nav>
   </div>
 </template>
--- components/Brands.vue
+++ components/Brands.vue
@@ -1,16 +1,14 @@
 <script setup lang="ts">
-
+const brandList = ["Filippo Sorcinelli", "Etat Libre d'Orange", "Дискавери сэты", "Услуги / Встречи", "Все"];
+// Остатки во флаконах
 </script>
 
+
 <template>
-    <section class="w-[320px] h-[400px] bg-[#FFF1E8] border-4 border-black rounded-xl max-[801px]:hidden">
+    <section class="min-w-[220px] h-[400px] bg-[#FFF1E8] border-4 border-black rounded-xl max-[1001px]:hidden">
         <h3 class="border-b-2 border-black py-2 text-lg text-center">Все Бренды</h3>
         <ul class="mt-2 text-center">
-            <li>Filippo Sorcinelli</li>
-            <li>Etat Libre d'Orange</li>
-            <li>Orto Parisi</li>
-            <li>Остатки во флаконах</li>
-            <li>Услуги / Встречи</li>
+            <li v-for="(brands, index) in brandList" :key="index" class="py-1 border-b-2 border-black">{{ brands }}</li>
         </ul>
     </section>
 </template>
