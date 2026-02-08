6b7a0e5972047b33f849c72076c50e7ead4244cb
umbra <vitali.slepnyov@gmail.com>
2026-02-08 23:46:15 +0300

    feat:  настройка кнопок навигации

--- app/app.vue
+++ app/app.vue
@@ -8,23 +8,12 @@
     <main class="flex w-[clamp(500px,99vw,1800px)] max-[801px]:m-0 m-4 overflow-hidden">
 
       <!-- ! Бренды -->
+      <!-- <Brands /> -->
       <Brands />
 
-      <!-- <section class="min-w-[220px] h-[400px] bg-[#FFF1E8] border-4 border-black rounded-xl max-[1001px]:hidden">
-        <h3 class="border-b-2 border-black py-2 text-lg text-center">Все Бренды</h3>
-        <ul class="mt-2 text-center">
-          <li>Filippo Sorcinelli</li>
-          <li class="hidden">Etat Libre d'Orange</li>
-          <li>Наборы пробников</li>
-          <li>Остатки во флаконах</li>
-          <li class="hidden">Услуги / Встречи</li>
-          <li>Все</li>
-        </ul>
-      </section> -->
-
       <!-- ! Магазин -->
-      <Shop />
-
+      <!-- <Shop /> -->
+      <component :is="viewComponents[currentView]" />
 
       <!-- ! Корзина -->
       <Cart />
@@ -33,10 +22,10 @@
     </main>
     <nav
       class="flex bottom-0 w-screen justify-around fixed text-lg pb-8 pt-4 bg-[#FFB904] min-[1001px]:hidden border-t-4 border-black">
-      <button @click="" class="mx-4">Бренды</button>
-      <button @click="" class="">Магазин</button>
-      <button @click="" class="ml-4">Корзина</button>
-      <button @click="" class="mx-4">Профиль</button>
+      <button @click="setView('Brands')" class="mx-4">Бренды</button>
+      <button @click="setView('Shop')" class="">Магазин</button>
+      <button @click="setView('Cart')" class="ml-4">Корзина</button>
+      <button @click="setView('Profile')" class="mx-4">Профиль</button>
     </nav>
   </div>
 </template>
@@ -44,9 +33,32 @@
 
 
 <script setup lang="ts">
-import Shop from '../components/Shop.vue';
-import Brands from '../components/Brands.vue'
-import Cart from '../components/ShoppingCart.vue'
 
-const imageUrl = 'https://lulua.pl/wp-content/uploads/2024/10/perfumeria_lulua_naked_laundry_btso-900x900.jpg';
+import { ref } from 'vue';
+import Shop from '../components/Shop.vue';
+import Brands from '../components/Brands.vue';
+import Cart from '../components/ShoppingCart.vue';
+import Profile from '../components/Profile.vue'
+
+
+// Тип для названий вкладок
+type ViewName = 'Brands' | 'Shop' | 'Cart' | 'Profile';
+
+// Карта компонентов (для строгой типизации)
+const viewComponents = {
+  Brands,
+  Shop,
+  Cart,
+  Profile,
+} as const;
+
+// Текущее активное представление (типизированное)
+const currentView = ref<ViewName>('Shop');
+
+// Функция для переключения, с типом аргумента
+function setView(view: ViewName): void {
+  currentView.value = view
+}
+
+
 </script>
