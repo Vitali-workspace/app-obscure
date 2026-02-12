0490db5cc538ebfbe468eabfc53b51d0b6ea4353
umbra <vitali.slepnyov@gmail.com>
2026-02-12 23:53:23 +0300

    refactor:  изменить список брендов

--- components/Brands.vue
+++ components/Brands.vue
@@ -1,14 +1,31 @@
 <script setup lang="ts">
-const brandList = ["Filippo Sorcinelli", "Etat Libre d'Orange", "Дискавери сэты", "Услуги / Встречи", "Все"];
-// Остатки во флаконах
+//import { listFilippoSorcinelli } from '~~/utils/constants';
+
 </script>
 
 
 <template>
-    <section class="min-w-[220px] h-[400px] bg-[#FFF1E8] border-4 border-black rounded-xl max-[1001px]:hidden">
-        <h3 class="border-b-2 border-black py-2 text-lg text-center">Все Бренды</h3>
-        <ul class="mt-2 text-center">
-            <li v-for="(brands, index) in brandList" :key="index" class="py-1 border-b-2 border-black">{{ brands }}</li>
+    <section class="min-w-[240px] h-[440px] bg-[#FFF1E8] border-4 border-black rounded-xl max-[1001px]:hidden">
+        <h3 class="border-b-4 border-black py-2 text-lg text-center">Все Бренды - Уровни</h3>
+
+        <ul class="text-center">
+            <li class="py-2 border-b-2 border-black">Ванильный (Tier F)</li>
+            <li class="py-2 border-b-2 border-black">Казуальный (Tier D)</li>
+            <li class="py-2 border-b-2 border-black">Странный (Tier C)</li>
+            <li class="py-2 border-b-2 border-black">Эксперементальный (TierB)</li>
+            <li class="py-2 border-b-2 border-black">Хардкорный (TierA)</li>
+            <li class="py-2">Экстримальный (Tier S)</li>
+            <li class="py-2 hidden">Неопределённый (Tier IDK)</li>
+
+            <li class="border-t-4 border-b-2 border-black py-2">Наборы пробников</li>
+            <li class="py-2 border-b-2 border-black">Услуги / Встречи</li>
+            <li class="py-2">Остатки во флаконах</li>
         </ul>
+
+        <!-- <ul class="">
+            <li class="border-t-4 border-b-2 border-black py-2">Наборы пробников</li>
+            <li class="py-2 border-b-2 border-black">Услуги / Встречи</li>
+            <li class="py-2">Остатки во флаконах</li>
+        </ul> -->
     </section>
 </template>
