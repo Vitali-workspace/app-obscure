cf64a1b3883f9bf92ba0294fb34718b419a95083
umbra <vitali.slepnyov@gmail.com>
2026-02-16 23:09:27 +0300

    feat:  добавить библиотеку нот

---
+++ utils/nots.ts
@@ -0,0 +1,570 @@
+// animalistic
+const amber = "../assets/notes/animalistic/amber.webp";
+const ambrette = "../assets/notes/animalistic/ambrette.webp";
+const animalNotes = "../assets/notes/animalistic/animal-notes.webp";
+const blood = "../assets/notes/animalistic/blood.jpg";
+const body = "../assets/notes/animalistic/body.jpg";
+const bodyOdor = "../assets/notes/animalistic/body-odor.jpg";
+const castoreum = "../assets/notes/animalistic/castoreum.webp";
+const civet = "../assets/notes/animalistic/civet.webp";
+const fur = "../assets/notes/animalistic/fur.jpg";
+const hyraceum = "../assets/notes/animalistic/hyraceum.webp";
+const leather = "../assets/notes/animalistic/leather.webp";
+const lymph = "../assets/notes/animalistic/lymph.jpg";
+const musk = "../assets/notes/animalistic/musk.webp";
+const nocturnalAnimals = "../assets/notes/animalistic/nocturnal-animals.jpg";
+const pus = "../assets/notes/animalistic/pus.jpg";
+const saliva = "../assets/notes/animalistic/saliva.jpg";
+const skin = "../assets/notes/animalistic/skin.webp";
+const suede = "../assets/notes/animalistic/suede.webp";
+const sweat = "../assets/notes/animalistic/sweat.jpg";
+const urine = "../assets/notes/animalistic/urine.jpg";
+const vomit = "../assets/notes/animalistic/vomit.jpg";
+
+const animalistic = {
+  amber,
+  ambrette,
+  animalNotes,
+  blood,
+  body,
+  bodyOdor,
+  castoreum,
+  civet,
+  fur,
+  hyraceum,
+  leather,
+  lymph,
+  musk,
+  nocturnalAnimals,
+  pus,
+  saliva,
+  skin,
+  suede,
+  sweat,
+  urine,
+  vomit,
+};
+
+// balsams-oil
+const absoluteLabdanum = "../assets/notes/balsams-oil/absolute-labdanum.jpg";
+const absolutePlants = "../assets/notes/balsams-oil/absolute-plants.jpg";
+const copaibaBalsam = "../assets/notes/balsams-oil/copaiba-balsam.webp";
+const cypriolOil = "../assets/notes/balsams-oil/cypriol-oil.webp";
+const firBalsam = "../assets/notes/balsams-oil/fir-balsam.webp";
+const gurjunBalsam = "../assets/notes/balsams-oil/gurjun-balsam.webp";
+const peruBalsam = "../assets/notes/balsams-oil/peru-balsam.webp";
+const woodOil = "../assets/notes/balsams-oil/wood-oil.webp";
+
+const balsamsOil = {
+  absoluteLabdanum,
+  absolutePlants,
+  copaibaBalsam,
+  cypriolOil,
+  firBalsam,
+  gurjunBalsam,
+  peruBalsam,
+  woodOil,
+};
+
+// berries
+const blackberry = "../assets/notes/berries/blackberry.webp";
+const blackCherry = "../assets/notes/berries/black-cherry.webp";
+const blackCurrant = "../assets/notes/berries/black-currant.webp";
+const mulberry = "../assets/notes/berries/mulberry.webp";
+const physalis = "../assets/notes/berries/physalis.jpg";
+const redBerries = "../assets/notes/berries/red-berries.webp";
+
+const berries = {
+  blackberry,
+  blackCherry,
+  blackCurrant,
+  mulberry,
+  physalis,
+  redBerries,
+};
+
+// drinks
+const beer = "../assets/notes/drinks/beer.webp";
+const bourbonWhiskey = "../assets/notes/drinks/bourbon-whiskey.webp";
+const champagne = "../assets/notes/drinks/champagne.webp";
+const cocaCola = "../assets/notes/drinks/coca-cola.webp";
+const coffee = "../assets/notes/drinks/coffee.webp";
+const goatMilk = "../assets/notes/drinks/goat-s-milk.webp";
+const greenCoffee = "../assets/notes/drinks/green-coffee.webp";
+const liquor = "../assets/notes/drinks/liquor.webp";
+const mapleSyrup = "../assets/notes/drinks/maple-syrup.webp";
+const mate = "../assets/notes/drinks/mate.webp";
+const milk = "../assets/notes/drinks/milk.webp";
+const rum = "../assets/notes/drinks/rum.webp";
+const syrup = "../assets/notes/drinks/syrup.webp";
+
+const drinks = {
+  beer,
+  bourbonWhiskey,
+  champagne,
+  cocaCola,
+  coffee,
+  goatMilk,
+  greenCoffee,
+  liquor,
+  mapleSyrup,
+  mate,
+  milk,
+  rum,
+  syrup,
+};
+
+// flowers
+const cistus = "../assets/notes/flowers/cistus.jpg";
+const coconutBlossom = "../assets/notes/flowers/coconut-blossom.webp";
+const datura = "../assets/notes/flowers/datura.webp";
+const floralNotes = "../assets/notes/flowers/floral-notes.webp";
+const freesia = "../assets/notes/flowers/freesia.webp";
+const geranium = "../assets/notes/flowers/geranium.webp";
+const heliotrope = "../assets/notes/flowers/heliotrope.webp";
+const hyacinth = "../assets/notes/flowers/hyacinth.webp";
+const iris = "../assets/notes/flowers/iris.webp";
+const jasmine = "../assets/notes/flowers/jasmine.webp";
+const lavender = "../assets/notes/flowers/lavender.webp";
+const lilyValley = "../assets/notes/flowers/lily-of-the-valley.webp";
+const magnolia = "../assets/notes/flowers/magnolia.webp";
+const narcissus = "../assets/notes/flowers/narcissus.webp";
+const neroli = "../assets/notes/flowers/neroli.webp";
+const nightBlooming = "../assets/notes/flowers/night-blooming-jasmine.webp";
+const orangeBlossom = "../assets/notes/flowers/orange-blossom.webp";
+const osmanthus = "../assets/notes/flowers/osmanthus.webp";
+const rose = "../assets/notes/flowers/rose.webp";
+const tiareFlower = "../assets/notes/flowers/tiare-flower.webp";
+const turkishRose = "../assets/notes/flowers/turkish-rose.webp";
+const violet = "../assets/notes/flowers/violet.webp";
+const ylangYlang = "../assets/notes/flowers/ylang-ylang.webp";
+
+const flowers = {
+  cistus,
+  coconutBlossom,
+  datura,
+  floralNotes,
+  freesia,
+  geranium,
+  heliotrope,
+  hyacinth,
+  iris,
+  jasmine,
+  lavender,
+  lilyValley,
+  magnolia,
+  narcissus,
+  neroli,
+  nightBlooming,
+  orangeBlossom,
+  osmanthus,
+  rose,
+  tiareFlower,
+  turkishRose,
+  violet,
+  ylangYlang,
+};
+
+// fruits
+const apple = "../assets/notes/fruits/apple.webp";
+const banana = "../assets/notes/fruits/banana.webp";
+const bergamot = "../assets/notes/fruits/bergamot.webp";
+const bloodOrange = "../assets/notes/fruits/blood-orange.webp";
+const fig = "../assets/notes/fruits/fig.webp";
+const grapefruit = "../assets/notes/fruits/grapefruit.webp";
+const guava = "../assets/notes/fruits/guava.webp";
+const lemon = "../assets/notes/fruits/lemon.webp";
+const lemonZest = "../assets/notes/fruits/lemon-zest.webp";
+const lime = "../assets/notes/fruits/lime.webp";
+const mandarin = "../assets/notes/fruits/mandarin.webp";
+const mango = "../assets/notes/fruits/mango.webp";
+const melon = "../assets/notes/fruits/melon.webp";
+const orange = "../assets/notes/fruits/orange.webp";
+const papaya = "../assets/notes/fruits/papaya.webp";
+const pineapple = "../assets/notes/fruits/pineapple.webp";
+const plum = "../assets/notes/fruits/plum.webp";
+
+const fruits = {
+  apple,
+  banana,
+  bergamot,
+  bloodOrange,
+  fig,
+  grapefruit,
+  guava,
+  lemon,
+  lemonZest,
+  lime,
+  mandarin,
+  mango,
+  melon,
+  orange,
+  papaya,
+  pineapple,
+  plum,
+};
+
+// gourmand
+const bran = "../assets/notes/gourmand/bran.webp";
+const bread = "../assets/notes/gourmand/bread.webp";
+const butter = "../assets/notes/gourmand/butter.webp";
+const butterCream = "../assets/notes/gourmand/butter-cream.webp";
+const cake = "../assets/notes/gourmand/cake.webp";
+const caramel = "../assets/notes/gourmand/caramel.webp";
+const cookie = "../assets/notes/gourmand/cookie.webp";
+const cream = "../assets/notes/gourmand/cream.webp";
+const darkChocolate = "../assets/notes/gourmand/dark-chocolate.webp";
+const ediblePowder = "../assets/notes/gourmand/edible-powder.jpg";
+const honey = "../assets/notes/gourmand/honey.webp";
+const iceCream = "../assets/notes/gourmand/ice-cream.webp";
+const liveYeast = "../assets/notes/gourmand/live-yeast.webp";
+const milkMousse = "../assets/notes/gourmand/milk-mousse.webp";
+const nutella = "../assets/notes/gourmand/nutella.webp";
+const popcorn = "../assets/notes/gourmand/popcorn.webp";
+const ricePowder = "../assets/notes/gourmand/rice-powder.webp";
+const sugar = "../assets/notes/gourmand/sugar.webp";
+const whiteChocolate = "../assets/notes/gourmand/white-chocolate.webp";
+
+const gourmand = {
+  bran,
+  bread,
+  butter,
+  butterCream,
+  cake,
+  caramel,
+  cookie,
+  cream,
+  darkChocolate,
+  ediblePowder,
+  honey,
+  iceCream,
+  liveYeast,
+  milkMousse,
+  nutella,
+  popcorn,
+  ricePowder,
+  sugar,
+  whiteChocolate,
+};
+
+// green
+const angelica = "../assets/notes/green/angelica.webp";
+const blackberryLeaf = "../assets/notes/green/blackberry-leaf.webp";
+const blackCurrantLeaf = "../assets/notes/green/black-currant-leaf.webp";
+const calamus = "../assets/notes/green/calamus.webp";
+const cannabis = "../assets/notes/green/cannabis.webp";
+const clarySage = "../assets/notes/green/clary-sage.webp";
+const conifer = "../assets/notes/green/conifer.webp";
+const costus = "../assets/notes/green/costus.webp";
+const davana = "../assets/notes/green/davana.webp";
+const foliage = "../assets/notes/green/foliage.webp";
+const galbanum = "../assets/notes/green/galbanum.webp";
+const grass = "../assets/notes/green/grass.webp";
+const greenNotes = "../assets/notes/green/green-notes.webp";
+const hay = "../assets/notes/green/hay.webp";
+const immortelle = "../assets/notes/green/immortelle.webp";
+const juniper = "../assets/notes/green/juniper.webp";
+const laminaria = "../assets/notes/green/laminaria.webp";
+const mint = "../assets/notes/green/mint.webp";
+const moss = "../assets/notes/green/moss.webp";
+const myrtle = "../assets/notes/green/myrtle.webp";
+const nagarmotha = "../assets/notes/green/nagarmotha.jpg";
+const nard = "../assets/notes/green/nard.webp";
+const patchouli = "../assets/notes/green/patchouli.webp";
+const tobacco = "../assets/notes/green/tobacco.webp";
+const vetiver = "../assets/notes/green/vetiver.webp";
+const violetLeaf = "../assets/notes/green/violet-leaf.webp";
+const wormwood = "../assets/notes/green/wormwood.webp";
+
+const green = {
+  angelica,
+  blackberryLeaf,
+  blackCurrantLeaf,
+  calamus,
+  cannabis,
+  clarySage,
+  conifer,
+  costus,
+  davana,
+  foliage,
+  galbanum,
+  grass,
+  greenNotes,
+  hay,
+  immortelle,
+  juniper,
+  laminaria,
+  mint,
+  moss,
+  myrtle,
+  nagarmotha,
+  nard,
+  patchouli,
+  tobacco,
+  vetiver,
+  violetLeaf,
+  wormwood,
+};
+
+// mushrooms
+const fungus = "../assets/notes/mushrooms/fungus.jpg";
+const magicMushrooms = "../assets/notes/mushrooms/magic-mushrooms.jpg";
+const mold = "../assets/notes/mushrooms/mold.jpg";
+const mushroom = "../assets/notes/mushrooms/mushroom.jpg";
+const truffle = "../assets/notes/mushrooms/truffle.webp";
+
+const mushrooms = {
+  fungus,
+  magicMushrooms,
+  mold,
+  mushroom,
+  truffle,
+};
+
+// natural-atmospheric
+const clay = "../assets/notes/natural-atmospheric/clay.webp";
+const concrete = "../assets/notes/natural-atmospheric/concrete.webp";
+const dirt = "../assets/notes/natural-atmospheric/dirt.webp";
+const earthyNotes = "../assets/notes/natural-atmospheric/earthy-notes.webp";
+const iodine = "../assets/notes/natural-atmospheric/iodine.webp";
+const marineNotes = "../assets/notes/natural-atmospheric/marine-notes.webp";
+const metallicNotes = "../assets/notes/natural-atmospheric/metallic-notes.webp";
+const mud = "../assets/notes/natural-atmospheric/mud.webp";
+const ozone = "../assets/notes/natural-atmospheric/ozone.jpg";
+const petrichor = "../assets/notes/natural-atmospheric/petrichor.jpg";
+const rain = "../assets/notes/natural-atmospheric/rain.jpg";
+const salt = "../assets/notes/natural-atmospheric/salt.webp";
+const smoke = "../assets/notes/natural-atmospheric/smoke.webp";
+const solarNotes = "../assets/notes/natural-atmospheric/solar-notes.webp";
+const sulphur = "../assets/notes/natural-atmospheric/sulphur.webp";
+
+const natural = {
+  clay,
+  concrete,
+  dirt,
+  earthyNotes,
+  iodine,
+  marineNotes,
+  metallicNotes,
+  mud,
+  ozone,
+  petrichor,
+  rain,
+  salt,
+  smoke,
+  solarNotes,
+  sulphur,
+};
+
+// nuts
+const almond = "../assets/notes/nuts/almond.webp";
+const coconut = "../assets/notes/nuts/coconut.webp";
+const macadamia = "../assets/notes/nuts/macadamia.webp";
+const pistachio = "../assets/notes/nuts/pistachio.webp";
+
+const nuts = {
+  almond,
+  coconut,
+  macadamia,
+  pistachio,
+};
+
+// objects
+const adultToys = "../assets/notes/objects/adult-toys.jpg";
+const burntMatch = "../assets/notes/objects/burnt-match.webp";
+const carpet = "../assets/notes/objects/carpet.jpg";
+const causticSoda = "../assets/notes/objects/caustic-soda.webp";
+const dampRoom = "../assets/notes/objects/damp-room.jpg";
+const gasoline = "../assets/notes/objects/gasoline.webp";
+const gunpowder = "../assets/notes/objects/gunpowder.webp";
+const latex = "../assets/notes/objects/latex.webp";
+const lipstick = "../assets/notes/objects/lipstick.webp";
+const paper = "../assets/notes/objects/paper.webp";
+const plastic = "../assets/notes/objects/plastic.webp";
+const poppedBalloon = "../assets/notes/objects/popped-balloon.jpg";
+const poppers = "../assets/notes/objects/poppers.jpg";
+const sharpKnife = "../assets/notes/objects/sharp-knife.jpg";
+const soap = "../assets/notes/objects/soap.webp";
+const soapBlood = "../assets/notes/objects/soap-blood.webp";
+const tennisBall = "../assets/notes/objects/tennis-ball.webp";
+const wick = "../assets/notes/objects/wick.jpg";
+
+const objects = {
+  adultToys,
+  burntMatch,
+  carpet,
+  causticSoda,
+  dampRoom,
+  gasoline,
+  gunpowder,
+  latex,
+  lipstick,
+  paper,
+  plastic,
+  poppedBalloon,
+  poppers,
+  sharpKnife,
+  soap,
+  soapBlood,
+  tennisBall,
+  wick,
+};
+
+// resins
+const benzoin = "../assets/notes/resins/benzoin.webp";
+const elemi = "../assets/notes/resins/elemi.webp";
+const incense = "../assets/notes/resins/incense.webp";
+const labdanum = "../assets/notes/resins/labdanum.webp";
+const myrrh = "../assets/notes/resins/myrrh.webp";
+const opoponax = "../assets/notes/resins/opoponax.webp";
+const propolis = "../assets/notes/resins/propolis.webp";
+const styrax = "../assets/notes/resins/styrax.webp";
+
+const resins = {
+  benzoin,
+  elemi,
+  incense,
+  labdanum,
+  myrrh,
+  opoponax,
+  propolis,
+  styrax,
+};
+
+// spices
+const blackPepper = "../assets/notes/spices/black-pepper.webp";
+const caraway = "../assets/notes/spices/caraway.webp";
+const cardamom = "../assets/notes/spices/cardamom.webp";
+const cinnamon = "../assets/notes/spices/cinnamon.webp";
+const cloves = "../assets/notes/spices/cloves.webp";
+const coriander = "../assets/notes/spices/coriander.webp";
+const cumin = "../assets/notes/spices/cumin.webp";
+const fenugreek = "../assets/notes/spices/fenugreek.webp";
+const ginger = "../assets/notes/spices/ginger.webp";
+const nutmeg = "../assets/notes/spices/nutmeg.webp";
+const pinkPepper = "../assets/notes/spices/pink-pepper.webp";
+const rosemary = "../assets/notes/spices/rosemary.webp";
+const saffron = "../assets/notes/spices/saffron.webp";
+const spicyNotes = "../assets/notes/spices/spicy-notes.webp";
+const thyme = "../assets/notes/spices/thyme.webp";
+const tonkaBean = "../assets/notes/spices/tonka-bean.webp";
+const vanilla = "../assets/notes/spices/vanilla.webp";
+const whitePepper = "../assets/notes/spices/white-pepper.jpg";
+
+const spices = {
+  blackPepper,
+  caraway,
+  cardamom,
+  cinnamon,
+  cloves,
+  coriander,
+  cumin,
+  fenugreek,
+  ginger,
+  nutmeg,
+  pinkPepper,
+  rosemary,
+  saffron,
+  spicyNotes,
+  thyme,
+  tonkaBean,
+  vanilla,
+  whitePepper,
+};
+
+// synthetics
+const aldehydes = "../assets/notes/synthetics/aldehydes.webp";
+const calone = "../assets/notes/synthetics/calone.webp";
+const cashmeran = "../assets/notes/synthetics/cashmeran.webp";
+const diviniris = "../assets/notes/synthetics/diviniris.webp";
+const indole = "../assets/notes/synthetics/indole.webp";
+
+const synthetics = {
+  aldehydes,
+  calone,
+  cashmeran,
+  diviniris,
+  indole,
+};
+
+// vegetables
+const beetroot = "../assets/notes/vegetables-cereals/beetroot.webp";
+const carrotSeeds = "../assets/notes/vegetables-cereals/carrot-seeds.webp";
+const malt = "../assets/notes/vegetables-cereals/malt.webp";
+const potatoes = "../assets/notes/vegetables-cereals/potatoes.webp";
+const rhubarb = "../assets/notes/vegetables-cereals/rhubarb.webp";
+const wheat = "../assets/notes/vegetables-cereals/wheat.webp";
+
+const vegetables = {
+  beetroot,
+  carrotSeeds,
+  malt,
+  potatoes,
+  rhubarb,
+  wheat,
+};
+
+// woods
+const acorn = "../assets/notes/woods/acorn.webp";
+const agarwoodOud = "../assets/notes/woods/agarwood-oud.webp";
+const amyris = "../assets/notes/woods/amyris.webp";
+const bark = "../assets/notes/woods/bark.webp";
+const birch = "../assets/notes/woods/birch.webp";
+const cascarilla = "../assets/notes/woods/cascarilla.webp";
+const cedarWood = "../assets/notes/woods/cedar-wood.webp";
+const clearwood = "../assets/notes/woods/clearwood.webp";
+const cypress = "../assets/notes/woods/cypress.webp";
+const dryWood = "../assets/notes/woods/dry-wood.webp";
+const ebonyTree = "../assets/notes/woods/ebony-tree.webp";
+const eucalyptus = "../assets/notes/woods/eucalyptus.webp";
+const forestTrees = "../assets/notes/woods/forest-trees.jpg";
+const guaiacWood = "../assets/notes/woods/guaiac-wood.webp";
+const hinokiWood = "../assets/notes/woods/hinoki-wood.webp";
+const masticTree = "../assets/notes/woods/mastic-tree.jpg";
+const palisander = "../assets/notes/woods/palisander-rosewood.webp"; // Rosewood
+const pine = "../assets/notes/woods/pine-tree.webp";
+const sandalwood = "../assets/notes/woods/sandalwood.webp";
+const woodyNotes = "../assets/notes/woods/woody-notes.webp";
+
+const woods = {
+  acorn,
+  agarwoodOud,
+  amyris,
+  bark,
+  birch,
+  cascarilla,
+  cedarWood,
+  clearwood,
+  cypress,
+  dryWood,
+  ebonyTree,
+  eucalyptus,
+  forestTrees,
+  guaiacWood,
+  hinokiWood,
+  masticTree,
+  palisander,
+  pine,
+  sandalwood,
+  woodyNotes,
+};
+
+export {
+  animalistic,
+  balsamsOil,
+  berries,
+  drinks,
+  flowers,
+  fruits,
+  gourmand,
+  green,
+  mushrooms,
+  natural,
+  nuts,
+  objects,
+  resins,
+  spices,
+  synthetics,
+  vegetables,
+  woods,
+};
